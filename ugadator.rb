bb4cb165be5e83accc988017ed52f303e45f8310
habibullin <rxletter@gmail.com>
2017-02-14 00:45:02 +0300

    broken code

--- ugadator.rb
+++ ugadator.rb
@@ -1,8 +1,8 @@
+# Это не константы, т.к. ниже я перезаписываю их значение
 min = 0
 max = 100
 
-
-print "\n\n\n\n
+puts "
 dP     dP                         dP            dP                     
 88     88                         88            88                     
 88     88 .d8888b. .d8888b. .d888b88 .d8888b. d8888P .d8888b. 88d888b. 
@@ -11,38 +11,39 @@ Y8.   .8P 88.  .88 88.  .88 88.  .88 88.  .88   88   88.  .88 88
 `Y88888P' `8888P88 `88888P8 `88888P8 `88888P8   dP   `88888P' dP       
                .88                                                     
            d8888P                                                      
-           
-Загадай число от #{min} до #{max}, а я его угадаю :)
-Нажми Enter как будешь готов.\n\n"
 
+Загадай число от #{min} до #{max}, а я его угадаю :)
+Нажми Enter как будешь готов."
 gets
 
-while (max - min) > 1 do
-	print "Это число больше чем #{(max + min) / 2}?"
-	a = gets.chomp
-	a.downcase!
+while range > 1 do
+  puts "Это число больше чем #{(max + min) / 2}?"
+  a = gets.chomp.downcase
 
-	if a.include? "y"
-		min = (((max + min) / 2) + 1)
-	elsif a.include? "n"
-		max = ((max + min) / 2)
-	else
-		print "Я понимаю только Yes/No или y/n.\n"
-	end
+  if a.include? "y"
+    min = (((max + min) / 2) + 1)
+  elsif a.include? "n"
+    max = ((max + min) / 2)
+  else
+    puts "Я понимаю только Yes/No или y/n."
+  end
 
-	# print "Debug: #{min}-#{max}\n"
+  # print "Debug: #{min}-#{max}\n"
 end
 
-if max - min == 0
-	print "Ты загадал число #{min} :)\n"
+if range == 0
+  puts "Ты загадал число #{min} :)"
 else
-	print "Это число #{min}?\n"
-	a = gets.chomp
-	a.downcase!
+  puts "Это число #{min}?"
+  a = gets.chomp.downcase
 
-	if a.include? "y"
-		print "Я крут! :)\n"
-	else
-		print "Ты загадал число #{max} :)\n"
-	end
+  if a.include? "y"
+    puts "Я крут! :)"
+  else
+    puts "Ты загадал число #{max} :)"
+  end
+end
+
+def range
+  max - min
 end
