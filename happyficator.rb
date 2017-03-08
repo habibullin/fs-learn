b987d17cf9ca44b53de980d20a8920a0a039d041
habibullin <rxletter@gmail.com>
2017-03-08 15:14:32 +0300

    with file read

--- happyficator.rb
+++ happyficator.rb
@@ -1,30 +1,58 @@
-class Number
-  def initialize(answer)
-    @answer = answer
+# class Number
+#   def initialize(answer)
+#     @answer = answer
 
-    while is_number? (@answer) or @answer.length != 6 #why not work if use '||' instead 'or'?
-      puts "Нужно ввести 6-значное число без букв или других символов"
-      @answer = gets.chomp
-    end
+#     while is_number? (@answer) or @answer.length != 6
+#       puts "Нужно ввести 6-значное число без букв или других символов"
+#       @answer = gets.chomp
+#     end
 
-    arr = @answer.scan(/.../).map {|e| e }
-    first = count (arr[0])
-    last = count (arr[1])
+#     arr = @answer.scan(/.../).map {|e| e }
+#     first = count (arr[0])
+#     last = count (arr[1])
 
-    first == last ? (puts "Счасливый билет :)") : (puts "Не повезло — это обычный билет :(")
-  end
+#     first == last ? (puts "Счасливый билет :)") : (puts "Не повезло — это обычный билет :(")
+#   end
 
-  def is_number? answer
-    !/\A\d+\z/.match(answer)
-  end
+#   def is_number? answer
+#     !/\A\d+\z/.match(answer)
+#   end
 
-  def count arr
-    n = 0
-    arr.split("").map(&:to_i).each { |i| n += i }
-    return n
-  end
+#   def count arr
+#     n = 0
+#     arr.split("").map(&:to_i).each { |i| n += i }
+#     return n
+#   end
+# end
+
+
+# puts "Введи 6-значный номер билета, а я скажу счастливый он или нет"
+# answer = Number.new(gets.chomp)
+
+
+def count number
+  n = 0
+  number.split("").map(&:to_i).each { |i| n += i }
+  return n
 end
 
+arr = File.read("out-randomizator.txt").split("\n")
 
-puts "Введи 6-значный номер билета, а я скажу счастливый он или нет"
-answer = Number.new(gets.chomp)
+arr.each { |x|
+  numb = x.scan(/.../).map {|e| e }
+  first = count (numb[0])
+  last = count (numb[1])
+  first == last ? (x.to_s << "_1") : (x.to_s << "_0")
+  # puts x
+  # x.scan(/.../).map {|e| e }
+  # puts x[0]
+  # puts x[1]
+  # puts "."
+  # first = count (x[0])
+  # last = count (x[1])
+  # first == last ? (x.to_s << "_1") : (x.to_s << "_0")
+}
+
+File.open('out-happyficator.txt', 'w'){ |file|
+    file.puts arr
+}
