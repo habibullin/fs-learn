6b1d1c7b477940ac6617e97d33a3c1586c0906ec
habibullin <rxletter@gmail.com>
2017-03-13 00:17:29 +0300

    back to step by step algorithm

--- randomizator.rb
+++ randomizator.rb
@@ -1,33 +1,32 @@
 #v1. 1_000_000 numbers 4.26 sec
+def time
+  start = Time.now.to_f
+  yield
+  elapsed = (Time.now.to_f - start).to_s
+  puts "Завершено за #{'%.2f' % elapsed} сек"
+end
 
-# def time
-#   start = Time.now.to_f
-#   yield
-#   elapsed = (Time.now.to_f - start).to_s
-#   puts "Завершено за #{'%.2f' % elapsed} сек"
-# end
-
-# time{
-#   File.open('out-randomizator.txt', 'w'){ |file|
-#     for i in 1..1_000_000
-#       file.puts rand(100_000..999_999)
-#     end
-#   }
-# }
+time{
+  File.open('out-randomizator.txt', 'w'){ |file|
+    for i in 1..100_000
+      file.puts rand(100_000..999_999)
+    end
+  }
+}
 
 
 #v2. 1_000_000 numbers 3.51 sec
-start = Time.now.to_f
+# start = Time.now.to_f
 
-my_array = []
+# my_array = []
 
-for i in 1..1_000_000
-  my_array << rand(100_000..999_999)
-end
+# for i in 1..1000
+#   my_array << rand(100_000..999_999)
+# end
 
-File.open('out-randomizator.txt', 'w'){ |file|
-    file.puts my_array
-}
+# File.open('out-randomizator.txt', 'w'){ |file|
+#     file.puts my_array
+# }
 
-elapsed = (Time.now.to_f - start).to_s
-puts "Завершено за #{'%.2f' % elapsed} сек"
+# elapsed = (Time.now.to_f - start).to_s
+# puts "Завершено за #{'%.2f' % elapsed} сек"
